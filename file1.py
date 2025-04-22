5896e4548204893799e6bfb605e8f6266a68edab
remytr <remytran56@icloud.com>
2025-04-22 13:38:52 +0100

    First commit

---
+++ file1.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+
+        assert (self.head_dim * heads == embed_size), "Embed size needs to be di by heads"
+
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias= False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads*self.head_dim, embed_size)
+
+    def forward(selfself, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        # Split embedding into self.heads pieces
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = query.reshape(N, key_len, self.heads, self.head_dim)
+
+        energy = torch.einsum()
+        # queries shape: (N, query_len, heads, head_dim)
+        # keys shape: (N, key_len, heads, heads_dim)
+
